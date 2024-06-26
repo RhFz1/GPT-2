659529586668f9aa833b305516fe15a4313e0c16
RhFz1 <nooralifirst@gmail.com>
2024-06-26 16:18:48 +0530

    Configure Fused AdamW for faster execution

--- train_gpt2.py
+++ train_gpt2.py
@@ -308,7 +308,7 @@ def get_lr(it):
 
     return min_lr + coeff * (max_lr - min_lr)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device_type=device)
 
 for i in range(max_steps):
     t0 = time.time()
