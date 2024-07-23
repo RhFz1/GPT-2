e0bd2c2846d4c4afb1f07bd63eca27c503423594
RhFz1 <nooralifirst@gmail.com>
2024-07-23 09:45:40 +0000

    Change code for data shards (Used Dr. Karpathy's)

--- dataloader.py
+++ dataloader.py
@@ -1,38 +1,56 @@
+import os
 import torch
-import tiktoken
+import numpy as np
 import torch.nn.functional as F
 import torch.utils
 from config import GPTConfig
 
+master_process = True
 # Sampling from shakespere
 class DataLoader():
-    def __init__(self, B, T, process_rank, num_processes):
+    def __init__(self, B, T, process_rank, num_processes, split):
 
         self.B = B
         self.T = T
         self.process_rank = process_rank
         self.num_processes = num_processes
+        assert split in {'train', 'val'}
+        
+        data_path = os.getenv('DATA_PATH') # Getting the datastore path
+        shards = os.listdir(data_path) # Listing the files in the datastore path
+        shards = [s for s in shards if split in s] # Filtering the files based on the split
+        shards = sorted(shards) # Sorting the files 
+        shards = [os.path.join(data_path, s) for s in shards] # Creating the full path of the files
+        
+        self.shards = shards
 
-        with open('input.txt', 'r') as file:
-            text = file.read()
-
-        tokenizer = tiktoken.get_encoding('gpt2')
-        self.tokens = torch.tensor(tokenizer.encode(text))
+        assert len(shards) > 0, f"no shards found for split {split}"
         if master_process:
-            print(f"Total number of tokens: {len(self.tokens)}")
-        self.current_pos = self.B * self.T * self.process_rank
+            print(f"found {len(shards)} shards for split {split}")
+        self.reset()
+
+    def load_tokens(self, filename):
+        npt = np.load(filename)
+        npt = npt.astype(np.int32) # added after video
+        ptt = torch.tensor(npt, dtype=torch.long)
+        return ptt
     
     def reset(self):
-        self.current_pos = torch.randint(len(self.tokens) - self.B * self.T, (1,)).item()
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = self.load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank
 
     def next_batch(self):
         B, T = self.B, self.T
-        buf = self.tokens[self.current_pos:self.current_pos + B * T + 1]
-        x = buf[:-1].view(B, T)
-        y = buf[1:].view(B, T)
-        self.current_pos += B * T * self.num_processes
-
-        if self.current_pos + B * T * self.num_processes + 1 > len(self.tokens):
-            self.current_pos = 0
-        
-        return x.to(GPTConfig.device), y.to(GPTConfig.device)
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B * T * self.num_processes
+        # if loading the next batch would be out of bounds, advance to next shard
+        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = self.load_tokens(self.shards[self.current_shard])
+            self.current_position = B * T * self.process_rank
+        return x, y
