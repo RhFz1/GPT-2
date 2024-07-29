414ea5768bb4f0d9342ba005e7d587074af09ba6
RhFz1 <nooralifirst@gmail.com>
2024-07-29 17:30:38 +0530

    Rename keys for loading model, will address bug later

--- inference.py
+++ inference.py
@@ -1,13 +1,17 @@
 import torch
 import os
-from traingpt2 import GPT, GPTConfig
+from model import GPT
+from config import GPTConfig
+from collections import OrderedDict
 
 model_path = '/home/syednoor/Desktop/FAIR/GPT-2/models'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = GPT(GPTConfig(vocab_size=50304))
 
 model = model.to(device)
-checkpoint = torch.load(os.path.join(model_path, 'ckpt_00.pt'), map_location=device)
+checkpoint = torch.load(os.path.join(model_path, 'ckpt_04.pt'), map_location=device)
+new_keys = {key: key.replace('_orig_mod.', '') for key in checkpoint['model'].keys()}
+checkpoint['model'] = OrderedDict((new_keys.get(k, k), v) for k, v in checkpoint['model'].items())
 
 model.load_state_dict(checkpoint['model'])
 
