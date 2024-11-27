e944c0dec0022d1237b7a4f8994282993313da4b
RhFz1 <nooralifirst@gmail.com>
2024-11-27 10:38:58 +0000

    Autocast for mixed precision training

--- main.py
+++ main.py
@@ -216,7 +216,8 @@ for i in range(50):
     x, y = trainloader.get_next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    logits, loss = model(x, y)
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
     torch.cuda.synchronize()
