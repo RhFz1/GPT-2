5e0223fb2b9c539fe23696fbb6c64497f9193693
RhFz1 <nooralifirst@gmail.com>
2024-11-27 08:32:22 +0000

    GPT2 init, as per paper

--- main.py
+++ main.py
@@ -90,6 +90,17 @@ class GPT(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
         self.transformer.wte.weight = self.lm_head.weight
+        
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, module):
+
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean = 0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight,mean = 0.0, std=0.02)
     
     def forward(self, idx: torch.Tensor, targets: torch.Tensor = None)->torch.Tensor:
         B, T = idx.size()
