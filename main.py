412b5aaf0f30be5c92d97ce6ba0abe042f65e112
RhFz1 <nooralifirst@gmail.com>
2024-11-28 04:16:59 +0000

    Use Flash Attention and increase vocab to a nice number

--- main.py
+++ main.py
@@ -53,10 +53,11 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         # attention (materializes the large (T,T) matrix for all the queries and keys)
-        att = (q @ k.transpose(-2, -1)) * self.scale
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        # att = (q @ k.transpose(-2, -1)) * self.scale
+        # att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        # att = F.softmax(att, dim=-1)
+        # y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
         # output projection
         y = self.c_proj(y)
@@ -200,12 +201,12 @@ elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
 
 print(f'Using device: {device}')
 
-model = GPT(ModelConfig())
+model = GPT(ModelConfig(vocab_size=50304))
 model.eval()
 model.to(device)
 model = torch.compile(model)
 
-B,T = 8, 1024
+B,T = 16, 1024
 trainloader = DataLoaderLite(B, T)
 
 torch.set_float32_matmul_precision('high')
